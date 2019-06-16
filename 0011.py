d3e88ca3d8ac6310ed279c48680dbd2ced1ca069
sqwii <zeliges@gmail.com>
2019-06-16 17:33:42 +0800

    add 0011

--- 0011.py
+++ 0011.py
@@ -1,3 +1,35 @@
 # 0011
 # open file
-file = open("")
+import os
+import jieba
+
+
+def getStopWord(base):
+    sw = []
+    # 打开文件
+    file = open(os.path.join(base, 'source/0011/filtered_words.txt'), 'r').readlines()
+    for word in file:
+        sw.append(word.split()[0])
+    
+    return sw
+
+def checkWord():
+    base = os.path.abspath('.')
+    sw = getStopWord(base)
+    word = input("please input your word:")
+    segList = jieba.cut_for_search(word)
+    # 判断元素
+    res = [i for i in list(segList)]
+    flag = 0
+    for se in res:
+        if se in sw:
+            print('Freedom')
+            flag = 1
+            break
+    if flag==0:
+        print('HumanRight')
+
+    
+    
+if __name__ == "__main__":
+    checkWord()
