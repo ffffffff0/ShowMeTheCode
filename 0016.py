9ec6fbf3ab383d6867d96b492b7da7f8c03c6057
sqwii <zeliges@gmail.com>
2019-06-16 17:59:35 +0800

    complete 0016

--- 0016.py
+++ 0016.py
@@ -1,1 +1,19 @@
 # 0016
+import pandas as pd 
+import os
+import ast
+
+
+def convertFile():
+    base = os.path.abspath('.')
+    text = open(os.path.join(base, 'source/0016/numbers.txt'), 'r').read()
+    textList = ast.literal_eval(text)
+    print(textList)
+    # 生成数据框
+    df = pd.DataFrame(textList)
+    print(df)
+    df.to_excel(os.path.join(base+'/source/0016', 'numbers.xls'), header=False, sheet_name='numbers', index=False)
+    
+
+if __name__ == "__main__":
+    convertFile()
