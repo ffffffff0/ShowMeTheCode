fa5a3a9131f439d74546d7b9a426dabc80288260
sqwii <zeliges@gmail.com>
2019-06-17 18:52:22 +0800

    add 0020

---
+++ 0020.py
@@ -0,0 +1,20 @@
+# 0020
+#%%
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import datetime
+import os
+
+# jupyter
+#%%
+# 读取文件
+base = os.path.abspath('.')
+df = pd.read_excel(os.path.join(base+'/source/0020', 'tra.xlsx'))
+df.head()
+
+#%%
+# 处理时间格式
+
+
+
+#%%
